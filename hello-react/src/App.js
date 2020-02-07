05fb9e0fbfd9b1446304f7c74ea1e9df39bc38b7
0pippi0 <j.romany91@gmail.com>
2020-02-07 16:02:10 +0900

    리액트 컴포넌트에서는 class 대신에 className 을 사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
+import './App.css'
 
 class App extends Component {
   render() {
-    const style = {
-      backgroundColor: 'black',
-      padding: '16px',
-      color: 'white',
-      fontSize: '12px'
-    };
-
     return (
-      <div style={style}>
-        hi there
+      <div className="App">
+        리액트
       </div>
     );
   }
