dbbf1c7c977a06c2098bcb50efd11b1bfb7bc240
0pippi0 <j.romany91@gmail.com>
2020-02-07 15:52:08 +0900

    조건부 렌더링 AND 연산자 조건이 true 일 때만 보여주고 false 경우 아무것도 보여주고 싶지 않을 때

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,9 +5,7 @@ class App extends Component {
     return (
       <div>
         {
-          1 + 1 === 2 
-            ? (<div>맞아요!</div>)
-            : (<div>틀려요!</div>)
+          1 + 1 === 2 && (<div>맞아요!</div>)
         }
       </div>
     );
