256ce9f165b3090c4e365091859972cb139d8e4e
0pippi0 <j.romany91@gmail.com>
2020-02-07 16:03:07 +0900

    주석 {/* ... */} 사이에 넣거나, 태그 사이에

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        리액트
+      <div>
+        {/* 주석은 이렇게 */}
+        <h1
+          // 태그 사이에
+        >리액트</h1>
       </div>
     );
   }
