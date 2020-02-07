bb90410fd10aad42079008a87d811cf946d6d152
0pippi0 <j.romany91@gmail.com>
2020-02-07 15:59:49 +0900

    스타일은 다음과 같이 작성

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    const value = 1;
+    const style = {
+      backgroundColor: 'black',
+      padding: '16px',
+      color: 'white',
+      fontSize: '12px'
+    };
+
     return (
-      <div>
-        {
-          (() => {
-            if (value === 1) return (<div>하나</div>);
-            if (value === 2) return (<div>둘</div>);
-            if (value === 3) return (<div>셋</div>);
-          })()
-        }
+      <div style={style}>
+        hi there
       </div>
     );
   }
