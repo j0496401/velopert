b026be55920e3c648c7229f8ce880a7c87900b07
0pippi0 <j.romany91@gmail.com>
2020-02-07 15:26:32 +0900

    Parsing error: Unterminated JSX contents 태그는 꼭 닫혀있어야 합니다. <div> 태그를 열었으면, </div> 를 통하여 태그를 꼭 닫아주어야 합니다

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,10 @@
-// src/App.js
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <input type="text">
       </div>
     );
   }
