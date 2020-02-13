14f8fb7025e375a8ee9f1de029beb2751e61f56b
0pippi0 <j.romany91@gmail.com>
2020-02-13 17:28:56 +0900

    PhoneInfo 쪽에서 삭제 기능을 구현 삭제 버튼을 만들어서 해당 버튼에 이벤트를 설정

--- phone-book/src/components/PhoneInfo.js
+++ phone-book/src/components/PhoneInfo.js
@@ -6,7 +6,13 @@ class PhoneInfo extends Component {
             name: '이름',
             phone: '010-0000-0000',
             id: 0
-        }
+        },
+    }
+
+    handleRemove = () => {
+        // 삭제 버튼이 클릭되면 onRemove 에 id 넣어서 호출
+        const { info, onRemove } = this.props;
+        onRemove(info.id);
     }
 
     render() {
@@ -17,13 +23,14 @@ class PhoneInfo extends Component {
         };
 
         const {
-            name, phone, id
+            name, phone
         } = this.props.info;
 
         return (
             <div style={style}>
                 <div><b>{name}</b></div>
                 <div>{phone}</div>
+                <button onClick={this.handleRemove}>삭제</button>
             </div>
         );
     }
