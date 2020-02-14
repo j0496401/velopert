a79d33765804151275ba01663cad150ead25e51b
0pippi0 <j.romany91@gmail.com>
2020-02-14 12:28:40 +0900

    데이터 수정 수정할때도 마찬가지로 불변성을 지켜줘야합니다 handleUpdate 라는 함수를 만들건데요, 이 함수는 id 와 data 라는 파라미터를 받아와서 필요한 정보를 업데이트
    handleUpdate 는 PhoneInfoList 의 onUpdate 로 전달

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -18,18 +18,32 @@ class App extends Component {
       }
     ]
   }
+
   handleCreate = (data) => {
     const { information } = this.state;    
     this.setState({
       information: information.concat({ id: this.id++, ...data })
     })
   }
+
   handleRemove = (id) => {
     const { information } = this.state;
     this.setState({
       information: information.filter(info => info.id !== id)
     })
   }
+
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+        ? { ...info, ...data } // 새 객체를 만들어서 기존의 값과 전달받은 data를 덮어씀
+        : info // 기존의 값을 그대로 유지
+      )
+    })
+  }
+
   render() {
     const { information } = this.state;
     return (
@@ -40,6 +54,7 @@ class App extends Component {
         <PhoneInfoList
           data={information}
           onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
         />
       </div>
     );
