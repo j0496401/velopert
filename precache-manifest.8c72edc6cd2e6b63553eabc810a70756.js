e1e6abc51d8b4363536b52b5eebb54112fc3a26d
0pippi0 <j.romany91@gmail.com>
2020-03-05 18:27:24 +0900

    Updates

---
+++ precache-manifest.8c72edc6cd2e6b63553eabc810a70756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d572f9ef49a08bda3700ef38dbbcd21",
+    "url": "/velopert/index.html"
+  },
+  {
+    "revision": "cef48d647809a4a08a43",
+    "url": "/velopert/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb18fc91aac9e2545139",
+    "url": "/velopert/static/js/2.46e80a36.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/velopert/static/js/2.46e80a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef48d647809a4a08a43",
+    "url": "/velopert/static/js/main.c703be2a.chunk.js"
+  },
+  {
+    "revision": "6bda93446f3320d28af9",
+    "url": "/velopert/static/js/runtime-main.182ae88a.js"
+  }
+]);
